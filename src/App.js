6e1e47594c888dbb7744b17e8cbdb4e65bd7db75
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-21 16:16:15 -0500

    Login-Register Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import UserForm from './components/LoginForm'
 
 function App() {
   return (
     <div className='App'>
       <h2>FoodTruck Trackr</h2>
+      <UserForm/>
     </div>
   );
 }
