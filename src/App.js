262ba32c899aa1ff6219d48af779d9b026bfae81
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-10-22 16:52:43 -0500

    scss files added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import './App.css';
+import '../src/components/scss/app.scss';
 import UserForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import Favorites from './components/Favorites';
