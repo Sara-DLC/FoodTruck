02ad93de3a94e367aa7d2db21806a5beafdd4405
DimejiAre <olardi2@gmail.com>
2019-10-22 14:29:53 +0100

    create Menu component with styling

---
+++ src/components/Menu.js
@@ -0,0 +1,33 @@
+import React, {useEffect} from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/actionCreators';
+
+const Menu = (props) => {
+    const {getMenu, menu} = props
+
+    const id = props.match.params.id;
+
+    useEffect(()=>{
+        getMenu(id);
+    },[])
+
+    return (
+        <div className='menu-page'>
+            {menu?
+            menu.map(item => (
+                <div className='menu'>
+                    <p>Meal: {item.itemName}</p>
+                    <p>Description: {item.itemDescription}</p>
+                    <p>Avg Rating: {item.customerRatingAvg}</p>
+                    <p>Price: {item.itemPrice}</p>
+                </div>
+            ))
+            :null }
+        </div>
+    )
+}
+
+export default connect(
+    state => state,
+    actionCreators
+)(Menu);
