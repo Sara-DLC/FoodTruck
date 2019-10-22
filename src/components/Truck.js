de9dc04b292ee9591b6d6afa3fd7f2b51a6bba50
DimejiAre <olardi2@gmail.com>
2019-10-22 14:02:39 +0100

    create Truck component with basic styling

---
+++ src/components/Truck.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../state/actionCreators';
+import './scss/Truck.scss';
+
+const Truck = (props) => {
+    const {truck} = props;
+
+    return (
+        <div className='truck'>
+            <div className='truck-image'>
+                <img src={truck.image} alt='food truck'/>
+            </div>
+            <div className='truck-info'>
+                <p>Cuisine Type: {truck.cuisineType}</p>
+                <p>Average Customer Rating: {truck.customerRatingAvg}</p>
+                <p>Current Location: {truck.currentLocation.location}</p>
+                <p>Next Location: {truck.nextLocation.location}</p>
+            </div>
+            <div className='truck-button'>
+                <button>Generic</button>
+                <button>Generic2</button>
+            </div>
+        </div>
+    )
+
+}
+
+export default connect(
+    state => state,
+    actionCreators
+)(Truck);
